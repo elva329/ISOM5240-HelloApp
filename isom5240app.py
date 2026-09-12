deba48ae5b23cb5e684642c8e7cf15e9a03605d2
elva <jia_shen@126.com>
2026-09-12 17:16:22 +0800

    feat: updated typo in the code

--- isom5240app.py
+++ isom5240app.py
@@ -1,3 +1,3 @@
 import streamlit as st  # type: ignore
 st.write("ISOM5240")
-st.wriet("ISOM5240-Deep Learning Business Applications with Python")
+st.write("ISOM5240-Deep Learning Business Applications with Python")
